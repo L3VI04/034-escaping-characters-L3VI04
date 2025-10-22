1bf5c8b10ec6e26bbce203c207027145fab8732a
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-10-22 15:50:15 +0000

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Define a string variable named introduction
+const introduction = "Hello!\n\tMy name is Alex.\nHere's a single quote: '\nHere's a double quote: \"\nAnd here's a backslash: \\";
+
+// Assign the string to the innerHTML of the HTML element with the id result
+document.getElementById("result").innerHTML = introduction;
